b81322ec696f55cf6bf19fb9d29c8dd3f5b609ec
Noah <noah@hack.se>
2014-11-05 18:59:58 +0100

    Fix typos

--- php/MySQL.config.php
+++ php/MySQL.config.php
@@ -3,7 +3,7 @@
  * Protokollen MySQL settings
  */
 
-define('DB_HOST', 'localhost')
+define('DB_HOST', 'localhost');
 define('DB_USER', 'pk');
 define('DB_PASS', 'protokoll');
 define('DB_NAME', 'pk');
