ee524aa9b03b35e3a085bae6b85b9330d4ccefc3
Noah <noah@hack.se>
2014-11-05 20:28:48 +0100

    xargs parallelization

--- scanner/update-stuff.sh
+++ scanner/update-stuff.sh
@@ -3,10 +3,6 @@
 # Protokollen - update script
 #
 
+JOBS=4
 cd "$(dirname $0)" || exit 1
-./pk-list-services-as-tsv.php |while read entId svcId svcType hostname; do
-	F="$hostname".json
-	./check_http_primary.py "$hostname" > "$F" && ./pk-import-http-prefs.php "$F" && rm -f "$F"
-	../bin/sslprobe "$hostname" > "$F" 2>/dev/null && ./pk-import-tls-statuses.php "$svcId" "$F" && rm -f "$F"
-	../bin/sslprobe "www.$hostname" > "$F" 2>/dev/null && ./pk-import-tls-statuses.php "$svcId" "$F" && rm -f "$F"
-done
+./pk-list-services-as-tsv.php | xargs -L 1 -P "$JOBS" ./update-svc-host.sh
