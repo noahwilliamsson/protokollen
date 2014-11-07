adb66c44883681c99a7f144fbd82d7df546aa834
Noah <noah@hack.se>
2014-11-07 23:25:52 +0100

    Print service type and hostname while updating

--- scanner/update-svc-host.sh
+++ scanner/update-svc-host.sh
@@ -15,6 +15,7 @@ hostname="$4"
 F="$hostname"."$$".json
 
 cd "$(dirname $0)" || exit 1
+printf "%s\t%s\n" "$svcType" "$hostname"
 ./check_http_primary.py "$hostname" > "$F" && ./pk-import-http-prefs.php "$F" && rm -f "$F"
 ../bin/sslprobe "$hostname" > "$F" 2>/dev/null && ./pk-import-tls-statuses.php "$svcId" "$F" && rm -f "$F"
 ../bin/sslprobe "www.$hostname" > "$F" 2>/dev/null && ./pk-import-tls-statuses.php "$svcId" "$F" && rm -f "$F"
