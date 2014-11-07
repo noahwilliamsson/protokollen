e89162b89a6e68aba0d3c25cffe03697873217df
Noah <noah@hack.se>
2014-11-07 23:43:52 +0100

    Use Protokollen class instead of raw MySQL queries

--- mysql/pk-initialize-from-entities.php
+++ mysql/pk-initialize-from-entities.php
@@ -9,14 +9,9 @@ require_once('../php/Protokollen.class.php');
 
 $p = new Protokollen();
 
-$m = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-$m->set_charset(DB_CHARSET) or die("$m->error\n");
-
-$q = 'SELECT domain FROM entities WHERE domain IS NOT NULL ORDER BY id';
-$r = $m->query($q) or die("$m->error, SQL: $q\n");
-while($row = $r->fetch_object()) {
+foreach($p->listEntityDomains() as $domain) {
 	/* Load entity */
-	$e = $p->getEntityByDomain($row->domain);
+	$e = $p->getEntityByDomain($domain);
 
 	/* Add HTTP service */
 	$s = $p->addService($e->id, Protokollen::SERVICE_TYPE_HTTP, $e->domain, 'Webbsajt '. $e->org);
@@ -31,6 +26,3 @@ while($row = $r->fetch_object()) {
 	/* Not yet implemented - should this be MX pointers instead? */
 	$p->addServiceHostname($s->id, $e->domain_email);
 }
-
-$r->close();
-
