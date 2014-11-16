8fb317c1ecc469f3d0b58523bf21876ba2588a87
Noah <noah@hack.se>
2014-11-16 16:06:06 +0100

    Decrease concurrency

--- scanner/update-stuff.sh
+++ scanner/update-stuff.sh
@@ -3,7 +3,7 @@
 # Protokollen - update script
 #
 
-JOBS=20
+JOBS=12
 cd "$(dirname $0)" || exit 1
 ./pk-update-services.php DNS | xargs -L1 -P "$JOBS" ./pk-update-service.sh
 ./pk-update-services.php HTTP | xargs -L1 -P "$JOBS" ./pk-update-service.sh
