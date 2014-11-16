f10889725ff27f3be44c42059605b9599faf4ae6
Noah <noah@hack.se>
2014-11-16 15:45:24 +0100

    Use new update mechanism

--- scanner/update-stuff.sh
+++ scanner/update-stuff.sh
@@ -5,4 +5,8 @@
 
 JOBS=20
 cd "$(dirname $0)" || exit 1
-./pk-list-services-as-tsv.php | xargs -L 1 -P "$JOBS" ./update-svc-host.sh
+./pk-update-services.php DNS | xargs -L1 -P "$JOBS" ./pk-update-service.sh
+./pk-update-services.php HTTP | xargs -L1 -P "$JOBS" ./pk-update-service.sh
+./pk-update-services.php HTTPS | xargs -L1 -P "$JOBS" ./pk-update-service.sh
+./pk-update-services.php Webmail | xargs -L1 -P "$JOBS" ./pk-update-service.sh
+./pk-update-services.php SMTP | xargs -L1 -P "$JOBS" ./pk-update-service.sh
