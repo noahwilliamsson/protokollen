45fbcf14db8a0ec0e5fac45141cc1346b07603b7
Noah <noah@hack.se>
2014-11-17 17:57:59 +0100

    Use newer PHP classes

--- tools/pk-add-service.php
+++ tools/pk-add-service.php
@@ -4,8 +4,8 @@
  * Protokollen - add service to entity
  */
 
-require_once('../php/Protokollen.class.php');
-
+require_once('../php/ServiceGroup.class.php');
+mb_internal_encoding('utf-8');
 
 if($argc < 4)
 	die("Usage: ${argv[0]} <base domain> <service type> <service name> [hostname]\n");
@@ -13,32 +13,47 @@ if($argc < 4)
 $domain = trim($argv[1], '.');
 $serviceType = trim($argv[2], '.');
 $serviceName = trim($argv[3], '.');
-$hostname = $serviceName;
-if($argc == 5)
-	$hostname = trim($argv[4], '.');
 
-$p = new Protokollen();
+$hosts = array();
+$hostnames = array();
+for($i = 3; $i < $argc; $i++) {
+	$obj = new stdClass();
+	$obj->hostname = trim(mb_convert_case($argv[$i], MB_CASE_LOWER), '.');
+	$proto = strtolower($serviceType);
+	if($proto === 'webmail')
+		$proto = 'https';
+	switch($proto) {
+	case 'http': $obj->port = 80; break;
+	case 'smtp': $obj->port = 25; break;
+	case 'https': $obj->port = 443; break;
+	case 'dns': $obj->port = 53; break;
+	default: die("Unsupported protocol '$proto'\n"); break;
+	}
+	$obj->prio = 0;
+	$obj->protocol = $proto;
+
+	$hostnames[] = $obj->hostname .':'. $obj->port;
+
+	$hosts[] = $obj;
+}
+
+$p = new ServiceGroup();
 $e = $p->getEntityByDomain($domain);
+if(!$e)
+	die("Unknown base domain: $domain\n");
 
 $svc = $p->getServiceByName($e->id, $serviceType, $serviceName);
 if($svc === NULL) {
 	$svcId = $p->addService($e->id, $serviceType, $serviceName);
-	echo "Added new service '$serviceName' with type $serviceType and ID: $svcId\n";
-}
-else
-	$svcId = $svc->id;
-
-$hostnames = $p->listServiceHostnames($svcId);
-$found = FALSE;
-foreach($hostnames as $h) {
-	if(!strcasecmp($h->hostname, $hostname)) {
-		echo "Hostname '$hostname' is already known for this service\n";
-		$found = true;
-		break;
-	}
+	$svc = $p->getServiceById($svcId);
+	echo "- Created new '$serviceType' service '$serviceName' with ID: $svc->id\n";
 }
 
-if(!$found) {
-	$id = $p->addServiceHostname($svcId, $hostname);
-	echo "Hostname '$hostname' added as ID $id as child under domain '$domain' (entity id: $e->id)\n";
+$grp = $p->getServiceGroup($svc->id);
+if($grp !== NULL) {
+	die("Service group already exists with ID $grp->id\n");
 }
+
+$grpId = $p->addServiceGroup($svc->id, $hosts);
+$grp = $p->getServiceGroup($svc->id, $grpId);
+echo "- Added service group [". implode(', ', $hostnames) ."] with ID: $grp->id\n";
