3052899f7dbde358dbea92af808cda164ebbed42
Noah <noah@hack.se>
2014-11-18 19:40:13 +0100

    Decrease parallelization

--- scanner/update-stuff.sh
+++ scanner/update-stuff.sh
@@ -3,7 +3,7 @@
 # Protokollen - update script
 #
 
-JOBS=12
+JOBS=10
 cd "$(dirname $0)" || exit 1
 ./pk-update-services.php DNS | xargs -L1 -P "$JOBS" ./pk-update-service.sh
 ./pk-update-services.php HTTP | xargs -L1 -P "$JOBS" ./pk-update-service.sh
