5177dc7d71dfa382bb8e6f66d6d7f83612eb4fdf
Noah <noah@hack.se>
2014-11-24 17:13:54 +0100

    Call tool to update summary table during scan update

--- scanner/update-stuff.sh
+++ scanner/update-stuff.sh
@@ -6,7 +6,9 @@
 JOBS=10
 cd "$(dirname $0)" || exit 1
 ./pk-update-services.php DNS | xargs -L1 -P "$JOBS" ./pk-update-service.sh
+./pk-update-summary-table.php >/dev/null
 ./pk-update-services.php HTTP | xargs -L1 -P "$JOBS" ./pk-update-service.sh
 ./pk-update-services.php HTTPS | xargs -L1 -P "$JOBS" ./pk-update-service.sh
 ./pk-update-services.php Webmail | xargs -L1 -P "$JOBS" ./pk-update-service.sh
 ./pk-update-services.php SMTP | xargs -L1 -P "$JOBS" ./pk-update-service.sh
+./pk-update-summary-table.php >/dev/null
