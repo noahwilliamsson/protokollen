f1d841ca93ea64437ed115d1b8be49a66859cf05
Noah <noah@hack.se>
2014-12-09 05:33:13 +0100

    Allow charset and delimiter to be configured for CSV-file, making it compatible with Excel for Mac

--- web/reports.php
+++ web/reports.php
@@ -1,22 +1,56 @@
 <?php
 /**
  * Dump data in reports table as CSV
+ * Script args:
+ * - charset (UTF-8 or ISO-8859-1)
+ * - date (YYYY-mm-dd)
+ * - delim (CSV delimiter: ",", ";" or "\t")
  */
 
 require_once('../php/ProtokollenBase.class.php');
 
 $date = strftime('%F');
-if(isset($_GET['d']))
-	$date = strftime('%F', strtotime($_GET['d']));
+if(isset($_GET['date']))
+	$date = strftime('%F', strtotime($_GET['date']));
+
+$charset = 'UTF-8';
+$dbCharset = 'utf8';
+if(isset($_GET['charset'])) switch(strtoupper($_GET['charset'])) {
+case 'ISO-8859-1':
+case 'ISO-8859-15':
+case 'Windows-1252':
+case 'CP1252':
+	$charset = strtoupper($_GET['charset']);
+	$dbCharset = 'latin1';
+	break;
+case 'UTF-8':
+	$charset = strtoupper($_GET['charset']);
+	$dbCharset = 'utf8';
+	break;
+default:
+	break;
+}
+
+$delimiter = ',';
+if(isset($_GET['delim'])) switch($_GET['delim']) {
+case ',':
+case ';':
+case "\t":
+	$delimiter = $_GET['delim'];
+	break;
+default:
+	break;
+}
 
 $p = new ProtokollenBase();
 $m = $p->getMySQLHandle();
+$m->set_charset($dbCharset);
 $q = 'SELECT e.org, e.org_short, e.org_group, e.domain, e.domain_email, e.url, r.*
 FROM entities e LEFT JOIN reports r ON e.id=r.entity_id
 WHERE e.id > 1 AND r.created="'. $m->escape_string($date) .'"';
 
 $filename = 'protokollen-'. strftime('%Y%m%d_%H%m') .'.csv';
-header('Content-Type: text/csv; charset=utf-8');
+header('Content-Type: text/csv; charset='. $charset);
 header('Content-Disposition: attachment; filename='. $filename);
 
 $r = $m->query($q);
@@ -27,13 +61,13 @@ while($row = $r->fetch_object()) {
 		$arr = array();
 		foreach($row as $k => $v)
 			$arr[] = $k;
-		fputcsv($fd, $arr, "\t");
+		fputcsv($fd, $arr, $delimiter);
 		$didHeader = TRUE;
 	}
 	$arr = array();
 	foreach($row as $k => $v)
 		$arr[] = $v;
-	fputcsv($fd, $arr, "\t");
+	fputcsv($fd, $arr, $delimiter);
 }
 $r->close();
 fclose($fd);
